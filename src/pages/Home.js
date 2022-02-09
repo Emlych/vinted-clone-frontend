ef025ba1a48d82a59437fc23b67055e38089778f
Emily LD <lagreouemily@gmail.com>
2022-02-09 13:56:32 +0100

    Data fetch functionnal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,48 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import CardProduct from "../components/CardProduct";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Fetch data from Vinted API
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-vinted-api.herokuapp.com/offers"
+        );
+        setData(response.data);
+        setIsLoading(false);
+        console.log(data);
+      } catch (error) {
+        console.log(error.response);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <Header />
-      <div className="products">
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-      </div>
+      {isLoading ? (
+        <span>En cours de chargement...</span>
+      ) : (
+        <div className="products">
+          <Link to="/product">
+            <CardProduct />
+          </Link>
+          <Link to="/product">
+            <CardProduct />
+          </Link>
+          <Link to="/product">
+            <CardProduct />
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
